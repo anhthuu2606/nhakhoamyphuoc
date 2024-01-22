3f263185ad38242130c260246de353ffdb8e2ff8
anhthuu2606 <157344360+anhthuu2606@users.noreply.github.com>
2024-01-22 15:31:37 +0700

    Add files via upload

---
+++ js/control.js
@@ -0,0 +1,9 @@
+// JavaScript Document
+$('#cn').owlCarousel({
+  loop: true, //cho lap lai 
+  items: 1, // xuat hien 1 canh thoi
+  smartSpeed: 1000, //toc do thay doi anh
+  autoplay: true, //cho phep tu dong chay
+  autoplayTimeout: 5000, //thoi gian chờ khi chuyển ảnh khi chạy tự động 
+  nav: true, //cho xuất hiện bộ nút tới lui 
+})
